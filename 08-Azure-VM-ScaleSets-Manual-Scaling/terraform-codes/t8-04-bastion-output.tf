b1313de9384775012c69fa9b178e37636ff922ac
Doublex <moudoblex@yahoo.com>
2022-08-01 21:54:27 +0100

    update

--- 08-Azure-VM-ScaleSets-Manual-Scaling/terraform-codes/t8-04-bastion-output.tf
+++ 08-Azure-VM-ScaleSets-Manual-Scaling/terraform-codes/t8-04-bastion-output.tf
@@ -3,3 +3,6 @@ output "bastion_host_linuxvm_public_ip_address" {
   description = "Bastion Host Linux VM Public Adress"
   value = azurerm_public_ip.bastion_host_publicip.ip_address
 }
+output "baston_host_linuxvm" {
+  value = azurerm_linux_virtual_machine.baston_host_linuxvm.computer_name
+}
